98b5595daaeb23ff387ba7bd1fc0569127bc971b
Esteban <104602184+HyruleST@users.noreply.github.com>
2024-08-11 20:56:25 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,6 @@ sobre.addEventListener('click',()=>{
     sobreAbertura.style.transform = "rotate(0)";
     sobreAbertura.style.top = "0px"
     setTimeout(()=>{
-        window.location.href = "../html/secondPage.html"
+        window.location.href = "./html/secondPage.html"
     },1000)
 });
