57f679ef5c2fd260fdff22cee7d545c1fb0a8d96
Esteban <104602184+HyruleST@users.noreply.github.com>
2024-08-11 20:51:46 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const sobre = document.querySelector('#sobre');
+const sobreAbertura = document.querySelector('#sobre_copia');
+
+sobre.addEventListener('click',()=>{
+    sobreAbertura.style.transform = "rotate(0)";
+    sobreAbertura.style.top = "0px"
+    setTimeout(()=>{
+        window.location.href = "../html/secondPage.html"
+    },1000)
+});
